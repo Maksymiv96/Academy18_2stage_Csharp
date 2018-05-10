64b1c2b2b0998fe28c7ed9097f87d4887fbe60d7
Maksymiv96 <Maksymiv96@users.noreply.github.com>
2018-05-11 00:22:57 +0300

    updating parking to vvorking vers

--- Parking.cs
+++ Parking.cs
@@ -3,78 +3,146 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Threading;
+using System.IO;
 
 namespace Academy18_2stage_Csharp
 {
     class Parking
     {
-        private List<Transaction> transactions = new List<Transaction>();
-        private List<Car> cars = new List<Car>();
-        private double balance;
+        private static List<Transaction> transactions = new List<Transaction>();
+        private static List<Car> cars = new List<Car>();
+
+        internal static List<Transaction> Transactions { get => transactions; set => transactions = value; }
+        internal static List<Car> Cars { get => cars; set => cars = value; }
+
+        private static double balance;
         private double startbalance;
 
-        public double Balance { get => balance; }
+        public static double Balance { get => balance; set => balance = value; }
         public double Startbalance { get => startbalance; }
+
+        private static TimerCallback tm = new TimerCallback(takecharge);
+        private Timer _timer = new Timer(tm, null, 0, Setting.Timeout*1000);
+
+        private static TimerCallback tm2 = new TimerCallback(creatinglog);
+        private Timer _timer2 = new Timer(tm2, null, 0, 60 * 1000);
 
         private Parking()
         {
             try
             {
                 Console.WriteLine("set parking starting balance");
-                balance = Convert.ToInt32( Console.ReadLine());
+                Balance = Convert.ToInt32( Console.ReadLine());
                 
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message + "\nsetting initialization failed, set into standart");
-                balance = 1;
+                Balance = 1;
                 
                 //throw ;
             }
             finally
             {
-                startbalance = balance;
+                startbalance = Balance;
             }
         }
         
         private static readonly Lazy<Parking> Lazy = new Lazy<Parking>(() => new Parking());
         public static Parking Instance => Lazy.Value;
 
+        
+
+        private static void takecharge(object obj)
+        {
+            foreach (Car car in Cars)
+            {
+                if (car.Balance >= Setting.DictionaryGet(car.Type))
+                {
+                    Balance += Setting.DictionaryGet(car.Type);
+                    car.Balance -= Setting.DictionaryGet(car.Type);
+                }
+                else if (car.Balance > 0)
+                {
+                    Balance += car.Balance;
+                    car.Balance -= Setting.DictionaryGet(car.Type) * Setting.Fine;
+                }
+                else if (car.Balance <=0)
+                {
+                    car.Balance -= Setting.DictionaryGet(car.Type) * Setting.Fine;
+                }
+                //Console.WriteLine(car.Shovv());
+                //Console.WriteLine(balance);
+                Transactions.Add(new Transaction(car.Ident,Setting.DictionaryGet(car.Type)));
+            }
+        }
+
+
+        private static void creatinglog(object obj)
+        {
+            double sum = 0;
+            //Console.WriteLine("current time " + DateTime.Now);
+            foreach (Transaction trans in Transactions)
+            {
+                if (timetosec(trans.DateTime) > (timetosec(DateTime.Now) - 60))
+                {
+                    sum += trans.MOney;
+                }
+                    
+            }
+            string path = "transaction.log";
+            
+                using (StreamWriter sw = File.AppendText(path))
+                {
+                    sw.WriteLine($"{DateTime.Now}: total income for last minute: {sum}");
+
+
+                }
+           
+        }
+
+        static int timetosec(DateTime time)
+        {
+            DateTime dt = TimeZoneInfo.ConvertTimeToUtc(time);
+            DateTime dt2018 = new DateTime(2018, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            TimeSpan tsInterval = dt.Subtract(dt2018);
+            return Convert.ToInt32(tsInterval.TotalSeconds);
+        }
 
 
         public double gettotalearning()
         {
-            return Startbalance - Balance;
+            return Balance - Startbalance;
         }
         
 
 
         public void setintoparking (Car car)
         {
-            if (cars.Exists(x => x.Ident == car.Ident))
+            if (Cars.Exists(x => x.Ident == car.Ident))
             {
                 Console.WriteLine("Something vvrong, this car is already on the parking");
 
             }
             else
-            if (cars.Count() < Setting.Parkingspace)
-                cars.Add(car);
+            if (Cars.Count() < Setting.Parkingspace)
+                Cars.Add(car);
             else Console.WriteLine("Parking is full");
         }
 
         public void getfromparking (string IDcar)
         {
-            if (cars.Count() == 0) throw new Exception("Parking is empty");
-            if (!cars.Exists(x => x.Ident == IDcar))
+            if (Cars.Count() == 0) throw new Exception("Parking is empty");
+            if (!Cars.Exists(x => x.Ident == IDcar))
             {
                 throw new Exception("This car dosn't exists");
             }
             else
             {
-                int indexofcar = cars.FindIndex(x => x.Ident == IDcar);
-                if (cars[indexofcar].Balance >= 0)
-                    cars.RemoveAt(indexofcar);
+                int indexofcar = Cars.FindIndex(x => x.Ident == IDcar);
+                if (Cars[indexofcar].Balance >= 0)
+                    Cars.RemoveAt(indexofcar);
                 else throw new Exception("This car ovver, try refill your ballance");
             }
         }
