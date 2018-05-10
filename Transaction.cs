142b079e3ba98d22ab0fd45470a4ac78d9c97e87
Maksymiv96 <Maksymiv96@users.noreply.github.com>
2018-05-10 16:09:10 +0300

    Creating Transaction class

---
+++ Transaction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Academy18_2stage_Csharp
+{
+    class Transaction
+    {
+        DateTime dateTime;
+        string IDcar;
+        double MOney;
+        Transaction()
+        {
+
+        }
+    }
+}
